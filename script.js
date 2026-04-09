f7e7ec53b6f5c888ff8c0e954c7e590ef8fe0010
arslanalihaydar2006-jpg <arslanalihaydar2006@gmail.com>
2026-04-09 20:46:17 +0300

    İlk site dosyaları yapıldı

---
+++ script.js
@@ -0,0 +1,8 @@
+// Basit form kontrolü
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Başvurun gönderildi! Teşekkürler.");
+  });
+});
